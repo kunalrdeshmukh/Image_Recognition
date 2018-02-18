a6684bb21dfedd243528cab5084caabd00c54b9b
Kunal Deshmukh <kunaldeshmukh27@gmail.com>
2018-02-17 17:03:22 -0800

    trying to atchive results using python and go

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+
+using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char** argv )
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    string file_name;
+    cout<<"Enter Image file name: ";
+//    cin>>file_name ;
+     cv::Mat image ;
+    cout<<CV_MAJOR_VERSION<<":"<<CV_MINOR_VERSION;
 }
